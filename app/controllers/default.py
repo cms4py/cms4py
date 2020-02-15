cd790e98d634cb22f4a588684d7a6aeccc32aa10
梦宇 <xtiqin@163.com>
2020-02-15 17:14:38 +0800

    封装Response

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,15 +2,4 @@ from cms4py import http
 
 
 async def index(request: http.Request, response: http.Response):
-    await response._send({
-        'type': 'http.response.start',
-        'status': 200,
-        'headers': [
-                [b'content-type', b'text/html'],
-        ]
-    })
-    await response._send({
-        'type': 'http.response.body',
-        'body': b"Hello World",
-    })
-    pass
+    await response.write(b"Hello World")
--- cms4py/http.py
+++ cms4py/http.py
@@ -9,5 +9,36 @@ class Request:
 class Response:
     def __init__(self, send):
         self._send = send
+        self._content_type = b"text/html"
+        self._header_sent = False
         pass
-    pass
+
+    def _get_headers(self):
+        result = [
+            [b"content-type", self._content_type]
+        ]
+        return result
+
+    @property
+    def content_type(self) -> bytes:
+        return self._content_type
+
+    @content_type.setter
+    def content_type(self, value: bytes):
+        self._content_type = value
+
+    async def send_header(self, status=200):
+        await self._send({
+            'type': 'http.response.start',
+            'status': status,
+            'headers': self._get_headers()
+        })
+        self._header_sent = True
+
+    async def write(self, data: bytes):
+        if not self._header_sent:
+            await self.send_header()
+        await self._send({
+            "type": "http.response.body",
+            "body": data
+        })
