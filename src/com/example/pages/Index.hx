748565d5cb204a6948c6f039599802165a86ea8b
https://yunp.top <opengit@yunp.top>
2025-06-27 18:40:00 +0800

    实现几个常用功能

--- src/com/example/pages/Index.hx
+++ src/com/example/pages/Index.hx
@@ -7,14 +7,18 @@ import top.yunp.cms4py.web.routing.Page;
 
 @:build(hxasync.AsyncMacro.build())
 class Index extends Page {
-	public function new() {}
+    public function new() {}
 
-	@async override public function execute(request:Request):Response {
-		var r = @await useCursor(@async cursor -> {
-			// @await cursor.insert(db.users, {name: "OK"});
-			return @await cursor.select(db.users.id > 0);
-		});
-		Logger.info(r);
-		return templateResponse(request, "index.html", {content: "Hello"});
-	}
+    @async override public function execute(request:Request):Response {
+        var r = @await useCursor(@async cursor -> {
+            // @await cursor.insert(db.users, {name: "OK"});
+//            return @await cursor.select(db.users.id > 0);
+//            return @await cursor.delete(db.users.id == 4);
+//            return @await cursor.update(db.users.id == 2, {name:"Hello"});
+//            return @await cursor.selectOne(db.users.id == 2);
+            return @await cursor.select(db.users.id > 0, null, {limitby:[1, 2]});
+        });
+        Logger.info(r);
+        return templateResponse(request, "index.html", {content: "Hello"});
+    }
 }
--- src/top/yunp/cms4py/db/pydal/PQuery.hx
+++ src/top/yunp/cms4py/db/pydal/PQuery.hx
@@ -4,21 +4,33 @@ import top.yunp.cms4py.logger.Logger;
 import top.yunp.cms4py.lib.FuncTools;
 
 class PQuery {
-	private var _dalQuery:Dynamic;
+    private var _dalQuery:Dynamic;
 
-	public function new(dalQuery) {
-		_dalQuery = dalQuery;
-	}
+    public function new(dalQuery) {
+        _dalQuery = dalQuery;
+    }
 
-	public function select(?fields:Array<Dynamic>, ?options:Dynamic):String {
-		var sql = FuncTools.call(_dalQuery._select, fields, options);
-		Logger.debug(sql);
-		return sql;
-	}
+    public function select(?fields:Array<Dynamic>, ?options:Dynamic):String {
+        var sql = FuncTools.call(_dalQuery._select, fields, options);
+        Logger.debug(sql);
+        return sql;
+    }
 
-	public function count() {
-		var sql = _dalQuery._count();
-		Logger.debug(sql);
-		return sql;
-	}
+    public function count():String {
+        var sql = _dalQuery._count();
+        Logger.debug(sql);
+        return sql;
+    }
+
+    public function delete():String {
+        var sql = _dalQuery._delete();
+        Logger.debug(sql);
+        return sql;
+    }
+
+    public function update(data):String {
+        var sql = FuncTools.callNamed(_dalQuery._update, data);
+        Logger.debug(sql);
+        return sql;
+    }
 }
